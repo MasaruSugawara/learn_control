ecd14c1930aaee1cd9935b0f527d171615131fa4
Masaru Sugawara <masaru.sugawara@proxima-ai-tech.com>
2025-03-17 14:18:39 +0900

    [add] added compensator

--- ctrl_sim/ctrl_sim.py
+++ ctrl_sim/ctrl_sim.py
@@ -7,16 +7,42 @@ https://github.com/proxima-technology/casadi_mpc_nyuumon.git
 import casadi
 from .dynamical_system import *
 from .controller import *
+from abc import abstractmethod
+
+class Compensator:
+  def __init__(self, sys: Dynamical_System, ctrl: Controller):
+    self.sys = sys
+    self.ctrl = ctrl
+
+  @abstractmethod
+  def compensate(self, u: casadi.DM) -> casadi.DM:
+    pass
+
+class Passthrough_Compensator(Compensator):
+  """ Pass-through compensator
+
+  Trivial compensator which does nothing
+  """
+
+  def __init__(self, sys, ctrl):
+    super().__init__(sys, ctrl)
+
+  def compensate(self, u):
+    return u
 
 class Simulator:
   def __init__(self, sys: Dynamical_System, ctrl: Controller):
     self.sys = sys
     self.ctrl = ctrl
+    self.cpst = Passthrough_Compensator(sys, ctrl)
     self.history = []
 
   def set_aim(self, x_init, x_ref, u_ref):
     self.sys.set_init(x_init)
     self.ctrl.set_ref(x_ref, u_ref)
+
+  def set_compensator(self, comp: Compensator):
+    self.cpst = comp
   
   def advance_dt(self):
     """ advance the system by dt
@@ -28,13 +54,12 @@ class Simulator:
     self.ctrl.put_data(sys_state)
     u = self.ctrl.ctrl_out()
     if u is not None:
-      self.sys.update(u)
+      self.sys.update(self.cpst.compensate(u))
       t, x = sys_state
       self.history.append((t, x.full(), u.full()))
       return True
     else:
       return False
-  
   
   def execute(self, T: float, N: int):
     """ execute the system by time T as N steps
